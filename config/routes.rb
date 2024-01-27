291d2fb3b13783d3863e42d0a8e07599b4a376f8
root <root@localhost>
2024-01-27 23:28:23 +0000

    Changed small amount of routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@ Rails.application.routes.draw do
   root "books#index"
 
   get 'main/index'
-
-  # get 'books/index'
-  # get 'books/create'
-  # get 'books/read'
-  # get 'books/update'
-  # get 'books/delete'
   
   resources :books do
     member do
