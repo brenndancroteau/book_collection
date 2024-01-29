8f56ff3195dca00d8b9afbe96572b5302e51b0b7
root <root@localhost>
2024-01-29 01:22:35 +0000

    Finished Book Collection 2 with successful unit tests

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -7,11 +7,43 @@ RSpec.describe Book, type: :model do
   end
 
   it 'is valid with valid attributes' do
+    subject.title = 'test'
+    subject.author = 'test'
+    subject.price = 5.0
+    subject.published = '2024-01-01'
     expect(subject).to be_valid
   end
 
   it 'is not valid without a name' do
     subject.title = nil
+    subject.author = 'test'
+    subject.price = 5.0
+    subject.published = '2024-01-01'
     expect(subject).not_to be_valid
   end
+
+  it 'is not valid without an author' do
+    subject.title = 'test'
+    subject.author = nil
+    subject.price = 5.0
+    subject.published = '2024-01-01'
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.title = 'test'
+    subject.author = 'test'
+    subject.price = nil
+    subject.published = '2024-01-01'
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a published-date' do
+    subject.title = 'test'
+    subject.author = 'test'
+    subject.price = 5.0
+    subject.published = nil
+    expect(subject).not_to be_valid
+  end
+  
 end
