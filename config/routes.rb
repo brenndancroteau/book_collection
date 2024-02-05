dca2a1a3b92a7023d44677c1d99ff2e9b0c8657e
root <root@localhost>
2024-02-05 04:19:13 +0000

    add Procfile and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
 
-  root "books#index"
+  # root "books#index"
 
-  get 'main/index'
+  # get 'main/index'
   
-  resources :books do
-    member do
-      get :delete
-    end
-  end
+  # resources :books do
+  #   member do
+  #     get :delete
+  #   end
+  # end
+
+  resources :books
+  root 'books#index'
 
 end
