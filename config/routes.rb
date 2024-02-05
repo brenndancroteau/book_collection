543844e4982605d7ea6edfef20e4dca1e2ad2787
root <root@localhost>
2024-02-05 06:31:59 +0000

    Reset files to working state

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
 
+  root "books#index"
+
   get 'main/index'
   
-  # resources :books do
-  #   member do
-  #     get :delete
-  #   end
-  # end
-
-  resources :books
-  root 'books#index'
+  resources :books do
+    member do
+      get :delete
+    end
+  end
 
 end
